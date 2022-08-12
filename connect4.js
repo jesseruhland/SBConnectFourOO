02f87d7c8cee3783456a5c3f4ce776b31cd07bbf
Jesse Ruhland <jesse.ruhland@gmail.com>
2022-08-12 10:14:36 -0400

    assignment completed

--- connect4.js
+++ connect4.js
@@ -1,9 +1,29 @@
+class Player {
+  constructor(color) {
+    if (!color) {
+      this.color = `rgb(${(Math.floor(Math.random()*256))}, ${(Math.floor(Math.random()*256))}, ${(Math.floor(Math.random()*256))})`;
+    } else {
+      this.color = color
+    }
+    this.pId = `${Math.floor(Math.random()*100)}`
+  }
+}
+
 class Game {
-  constructor(HEIGHT = 6, WIDTH = 7) {
-    this.HEIGHT = HEIGHT;
-    this.WIDTH = WIDTH;
-    this.board = new Array();
-    this.currPlayer = 1;
+  constructor(HEIGHT, WIDTH, p1, p2) {
+    if(!HEIGHT || HEIGHT <= 0) {
+      this.HEIGHT = 6
+    } else {
+      this.HEIGHT = HEIGHT;
+    };
+    if (!WIDTH || WIDTH <= 0) {
+      this.WIDTH = 7
+    } else {
+      this.WIDTH = WIDTH;
+    } this.board = new Array();
+    this.players = new Array(p1, p2)
+    this.currPlayer = this.players[0];
+    this.gameOver = false
     this.makeBoard();
     this.makeHtmlBoard();
   }
@@ -53,7 +73,8 @@ class Game {
   placeInTable(y, x) {
     const piece = document.createElement('div');
     piece.classList.add('piece');
-    piece.classList.add(`p${this.currPlayer}`);
+    piece.classList.add(`p${this.currPlayer.pId}`);
+    piece.style.backgroundColor = this.currPlayer.color
     piece.style.top = -50 * (y + 2);
   
     const spot = document.getElementById(`${y}-${x}`);
@@ -61,8 +82,12 @@ class Game {
   }
   endGame(msg) {
     alert(msg);
+    this.gameOver = true;
   }
   handleClick(evt) {
+    if (this.gameOver) {
+      alert ("No more moves to be played, the game is over! Click 'OK' to start a new game.", location.reload())
+    }
     // get x from ID of clicked cell
     const x = +evt.target.id;
       
@@ -73,12 +98,12 @@ class Game {
     }
   
     // place piece in board and add to HTML table
-    this.board[y][x] = this.currPlayer;
+    this.board[y][x] = this.currPlayer.pId;
     this.placeInTable(y, x);
     
     // check for win
     if (this.checkForWin()) {
-      return this.endGame(`Player ${this.currPlayer} won!`);
+      return this.endGame(`Player ${this.currPlayer.color} won!`);
     }
     
     // check for tie
@@ -87,7 +112,7 @@ class Game {
     }
       
     // switch players
-    this.currPlayer = this.currPlayer === 1 ? 2 : 1;
+    this.currPlayer = this.currPlayer === this.players[0] ? this.players[1] : this.players[0];
   }
   checkForWin() {
       // Check four cells to see if they're all color of current player
@@ -100,7 +125,7 @@ class Game {
           y < this.HEIGHT &&
           x >= 0 &&
           x < this.WIDTH &&
-          this.board[y][x] === this.currPlayer
+          this.board[y][x] === this.currPlayer.pId
       );
 
     for (let y = 0; y < this.HEIGHT; y++) {
@@ -121,4 +146,19 @@ class Game {
   };
 }
 
-new Game(6,7);
+const newGame = (e) => {
+  const board = document.getElementById('board');
+  board.innerHTML='';
+  const player1Color = document.querySelector('#player1').value;
+  const player2Color = document.querySelector('#player2').value;
+  const rows = document.querySelector('#rows').value;
+  const columns = document.querySelector('#columns').value;
+  const p1 = new Player(player1Color);
+  const p2 = new Player(player2Color);
+  new Game(rows,columns,p1, p2)
+}
+
+const startButton = document.querySelector('#startBtn')
+startButton.addEventListener('click', newGame)
+
+//
